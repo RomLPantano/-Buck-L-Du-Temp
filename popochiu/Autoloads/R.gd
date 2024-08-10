79c00765a38aebc086b6dbe101357886b199978c
Sara Luna <violetfights@gmail.com>
2024-08-10 10:31:17 -0500

    Added menu room to project

--- popochiu/Autoloads/R.gd
+++ popochiu/Autoloads/R.gd
@@ -2,11 +2,14 @@ tool
 extends "res://addons/Popochiu/Engine/Interfaces/IRoom.gd"
 
 # classes ----
+const PR01MenuRoom := preload('res://popochiu/Rooms/01MenuRoom/Room01MenuRoom.gd')
 # ---- classes
 
 # nodes ----
+var 01MenuRoom: PR01MenuRoom setget , get_01MenuRoom
 # ---- nodes
 
 # functions ----
+func get_01MenuRoom(): return .get_runtime_room('01MenuRoom')
 # ---- functions
 
